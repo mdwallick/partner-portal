ad9f7acf866dfaf435a968092246bd893d6fe016
Mike Wallick <mdwallick@gmail.com>
2025-08-17 11:52:37 -0500

    initial commit

---
+++ lib/types.ts
@@ -0,0 +1,12 @@
+export type PartnerUser = {
+    id: string
+    partner_id: string
+    role: string
+    status: string
+    invited_at: string
+    joined_at: string
+    created_at: string
+    email: string
+    display_name: string
+    auth0_user_id: string
+}
